9205e8323dc9946d1b65931335af8a72a243b54e
Aleksey <omg.smsteel@gmail.com>
2014-10-16 14:31:35 +0400

    active items bugfix

--- Godville Bot.user.js
+++ Godville Bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Godville Bot
 // @namespace    http://godville.net/
-// @version      0.241
+// @version      0.242
 // @description  Godville Automatic Stuff
 // @author       UnstableFractal
 // @match        http://godville.net/superhero
